07024490545f9f74fb33ed9b9e7854a48aa894b8
LayaneB <layanejuvito@gmail.com>
2022-02-25 17:39:25 -0300

    adicionei duas funcionalidades a mais: poder jogar mais de uma vez (sem precisar recarregar a página) e um resumo com a quantidade de vitorias e empates ao fim do jogo

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -10,101 +10,115 @@
  * 
  * 
  */
-const mensagem =  `=============================
+alert(`=============================
           Boas vindas ao jogo Blackjack!
-=============================`;
+=============================`)
 
-const confirma = confirm(`${mensagem}
-Quer iniciar uma nova rodada?`);
-
-if (!confirma) {
-   console.log("O jogo acabou!")
-} else {
-   const incializaJogo = () => {
-      const usuario = [comprarCarta(), comprarCarta()];
-      return usuario;
-   }
-   // Definindo a mão dos jogadores;
-   let usuario = incializaJogo();
-   let computador = incializaJogo();
-
-   const somar = (num1, num2) => {
-      let soma = 0;
-      soma = num1 + num2;
-
-      return soma;
-   }
-   const cartas = (jogador) => {
-      const carta = jogador.map((carta) => {
-         return carta.texto;
-      })
-      return carta;
-   };
-
-   const pontuacao = (jogador) => {
-      const pontos = jogador.map((carta) => {
-         return carta.valor;
-      });
-      return pontos;
-   };
-
-
-   const verificaAs = (jogador) => {
-      let jogadorVerificado = jogador.map((array) => {
-         let usuario
-         if (array.texto[0].includes("A") && array.texto[1].includes("A")) {
-            usuario = incializaJogo();
-         } else {
-            usuario = array;
-         }
+let jogarNovamente = confirm(`Quer iniciar uma nova rodada?`);
+let vitoriasUsuario = 0;
+let vitoriasComputador = 0;
+let empate = 0;
+while (jogarNovamente) {
+      const incializaJogo = () => {
+         const usuario = [comprarCarta(), comprarCarta()];
          return usuario;
-      });
+      }
+      // Definindo a mão dos jogadores;
+      let usuario = incializaJogo();
+      let computador = incializaJogo();
 
-      return jogadorVerificado;
-   };
+      const somar = (num1, num2) => {
+         let soma = 0;
+         soma = num1 + num2;
 
-   let usuarioVerficado = verificaAs(usuario);
-   let computadorVerficado = verificaAs(computador);
+         return soma;
+      }
+      const cartas = (jogador) => {
+         const carta = jogador.map((carta) => {
+            return carta.texto;
+         })
+         return carta;
+      };
 
-   let cartasUsuario = cartas(usuarioVerficado);
-   let cartasComputador = cartas(computadorVerficado);
+      const pontuacao = (jogador) => {
+         const pontos = jogador.map((carta) => {
+            return carta.valor;
+         });
+         return pontos;
+      };
 
-   let pontosUsuario = pontuacao(usuarioVerficado);
-   let pontosComputador = pontuacao(computadorVerficado);
 
-   let pontuacaoUsuario = pontosUsuario[0]+ pontosUsuario[1];
-   let pontuacaoComputador = pontosComputador[0] + pontosComputador[1];
-   let compraMaisUma = true;
-   
+      const verificaAs = (jogador) => {
+         let jogadorVerificado = jogador.map((array) => {
+            let usuario
+            if (array.texto[0].includes("A") && array.texto[1].includes("A")) {
+               usuario = incializaJogo();
+            } else {
+               usuario = array;
+            }
+            return usuario;
+         });
 
-   while ((pontuacaoUsuario <= 21) && compraMaisUma) {
-      compraMaisUma = confirm(`Suas cartas são ${cartasUsuario.toString().replaceAll(",", " ")}. A carta revelada do computador é ${cartasComputador[0].toString().replace(",", " ")}
+         return jogadorVerificado;
+      };
+
+      let usuarioVerficado = verificaAs(usuario);
+      let computadorVerficado = verificaAs(computador);
+
+      let cartasUsuario = cartas(usuarioVerficado);
+      let cartasComputador = cartas(computadorVerficado);
+
+      let pontosUsuario = pontuacao(usuarioVerficado);
+      let pontosComputador = pontuacao(computadorVerficado);
+
+      let pontuacaoUsuario = pontosUsuario[0] + pontosUsuario[1];
+      let pontuacaoComputador = pontosComputador[0] + pontosComputador[1];
+      let compraMaisUma = true;
+
+
+      while ((pontuacaoUsuario <= 21) && compraMaisUma) {
+         compraMaisUma = confirm(`Suas cartas são ${cartasUsuario.toString().replaceAll(",", " ")}. A carta revelada do computador é ${cartasComputador[0].toString().replace(",", " ")}
 Deseja comprar mais uma carta? `);
 
-      if (compraMaisUma) {
-         const novaCarta = comprarCarta();
-         usuarioVerficado.push(novaCarta);
-         cartasUsuario = cartas(usuarioVerficado);
-         pontuacaoUsuario = somar(pontuacaoUsuario, novaCarta.valor);
-      } 
+         if (compraMaisUma) {
+            const novaCarta = comprarCarta();
+            usuarioVerficado.push(novaCarta);
+            cartasUsuario = cartas(usuarioVerficado);
+            pontuacaoUsuario = somar(pontuacaoUsuario, novaCarta.valor);
+         }
 
-   };
+      };
 
-
-   const vencedor = (pontosUsuario, pontosComputador) => {
-      if (pontosUsuario > pontosComputador && pontosUsuario <= 21) {
-         return `O usuário ganhou!`;
-      } else if (pontosUsuario === pontosComputador) {
-         return `Empate!`;
-      } else {
-         return `O computador ganhou!`;
+      
+      const vencedor = (pontosUsuario, pontosComputador) => {
+         if (pontosUsuario > pontosComputador && pontosUsuario <= 21) {
+            vitoriasUsuario ++;
+            return `O usuário ganhou!`;
+         } else if (pontosUsuario === pontosComputador) {
+            empate++;
+            return `Empate!`;
+         } else {
+            vitoriasComputador ++;
+            return `O computador ganhou!`;
+         }
       }
-   }
 
-   confirm(`Suas cartas são ${cartasUsuario.toString().replaceAll(",", " ")}. Sua pontuação é ${pontuacaoUsuario}
+      confirm(`Suas cartas são ${cartasUsuario.toString().replaceAll(",", " ")}. Sua pontuação é ${pontuacaoUsuario}
 As cartas do computador são ${cartasComputador.toString().replaceAll(",", " ")}. A pontuação do computador é ${pontuacaoComputador}
 ${vencedor(pontuacaoUsuario, pontuacaoComputador)}`);
 
 
+   // }
+
+   jogarNovamente = confirm("Deseja jogar novamente?");
+
 }
 
+if (!jogarNovamente) {
+      alert(`Foi muito bom jogar com você!
+=============================
+Você teve ${vitoriasUsuario} vitórias
+O computador venceu ${vitoriasComputador} vezes
+Tivemos um total de ${empate} empates
+=============================`)
+} 
