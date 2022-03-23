32197d3d8b6072de060f865cd353a410b5aa91ef
LayaneB <layanejuvito@gmail.com>
2022-03-23 17:07:44 -0300

    Atividade + desafios

---
+++ modulo2/estados-insta4/insta4/src/components/Post/Post.js
@@ -0,0 +1,190 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import iconeSalvoPreto from '../../img/turned_in_black.svg'
+import iconeSalvoBranco from '../../img/turned_in_not_black.svg'
+import iconeCompartilhar from '../../img/launch_black.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import { SecaoCompartilhar } from '../SecaoCompartilhar/SecaoCompartilhar'
+import iconeFacebook from '../../img/icons8-facebook.svg'
+import iconeInstagram from '../../img/icons8-instagram.svg'
+import iconeTwitter from '../../img/icons8-twitter-circled.svg'
+
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
+
+class Post extends React.Component {
+  state = {
+    curtido: false,
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0,
+    salvo:false,
+    compartilhar: false,
+  }
+
+  onClickCurtida = () => {
+    this.setState({
+      curtido:!this.state.curtido,
+    })
+    if(this.state.curtido){
+      this.setState({
+        numeroCurtidas: this.state.numeroCurtidas-1
+      }) 
+    }else{
+      this.setState({
+        numeroCurtidas: this.state.numeroCurtidas+1
+      })
+    }
+  }
+ 
+  onClickComentario = () => {
+    this.setState({
+      comentando: !this.state.comentando
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentando: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+  onClickSalvo = () => {
+    this.setState({
+      salvo:!this.state.salvo
+    })
+    console.log("O post foi salvo")
+  }
+
+  onClickCompartilhar = () => {
+    this.setState({
+      compartilhar:!this.state.compartilhar
+    })
+  }
+  onClickCompartilharFacebook = () =>{
+    this.setState({
+      compartilhar:!this.state.compartilhar
+    })
+    console.log("Post compartilhado no Facebookno com a mensagem: ")
+  }
+  onClickCompartilharInstagram = () =>{
+    this.setState({
+      compartilhar:!this.state.compartilhar
+    })
+    console.log("Post compartilhado no Instagram com a mensagem: ")
+  }
+  onClickCompartilharTwitter = () =>{
+    this.setState({
+      compartilhar:!this.state.compartilhar
+    })
+    console.log("Post compartilhado no Twitter com a mensagem: ")
+  }
+  render() {
+    let iconeCurtida
+
+    if(this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+    let iconeSalvo
+
+    if(this.state.salvo) {
+      iconeSalvo = iconeSalvoPreto
+    } else {
+      iconeSalvo = iconeSalvoBranco
+    }
+
+    let componenteComentario
+
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    }
+    let componenteCompartilhar
+
+    if(this.state.compartilhar) {
+      componenteCompartilhar = <SecaoCompartilhar
+      icone1={iconeFacebook}
+      icone2={iconeInstagram}
+      icone3={iconeTwitter}
+      onClickCompartilhar1={this.onClickCompartilharFacebook} 
+      onClickCompartilhar2={this.onClickCompartilharInstagram} 
+      onClickCompartilhar3={this.onClickCompartilharTwitter}
+      mensagem={this.mensagemCompartilhar}
+      />
+    }
+
+    return <PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+        <IconeComContador
+          icone={iconeCompartilhar}
+          onClickIcone={this.onClickCompartilhar}
+          valorContador={""}
+        />
+        <IconeComContador
+          icone={iconeSalvo}
+          onClickIcone={this.onClickSalvo}
+          valorContador={""}
+        />
+      </PostFooter>
+      {componenteComentario}
+      {componenteCompartilhar}
+    </PostContainer>
+  }
+}
+
+export default Post
---
+++ modulo2/estados-insta4/insta4/src/components/SecaoCompartilhar/SecaoCompartilhar.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const CompartilharContainer = styled.div`
+    display:flex;
+    flex-direction:column ;
+    align-items:center;
+    justify-content:center ;
+    
+`
+const OpcoesContainer = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:center ;
+
+`
+
+export class SecaoCompartilhar extends React.Component{
+    state={
+        mensagem:""
+    }
+    onChangeArea = (event) =>{
+        this.setState({
+            mensagem:event.target.value
+        })
+    }
+    onClickFace = ()=>{
+        this.props.onClickCompartilhar1()
+        console.log(this.state.mensagem)
+    }
+    onClickInsta = ()=>{
+        this.props.onClickCompartilhar2()
+        console.log(this.state.mensagem)
+    }
+    onClickTwitter = ()=>{
+        this.props.onClickCompartilhar3()
+        console.log(this.state.mensagem)
+    }
+    render(){
+        return(
+            <CompartilharContainer>
+                <div>
+                    <h4>Mensagem:</h4>
+                    <input value={this.props.mensagem} onChange={this.onChangeArea}></input>
+                </div>
+                <OpcoesContainer>
+                    <h4>Compartilhar com:</h4>
+                    <img 
+                        src={this.props.icone1} 
+                        onClick={this.onClickFace}
+                        alt={"facebook"} 
+                    />
+                    
+                    <img 
+                        src={this.props.icone2}
+                        onClick={this.onClickInsta}
+                        alt={"Instagram"}
+                    />
+
+                    <img
+                        src={this.props.icone3}
+                        onClick={this.onClickTwitter}
+                        alt={"Twitter"}
+                    />
+                </OpcoesContainer>
+                
+            </CompartilharContainer>
+        )
+    }
+        
+}
