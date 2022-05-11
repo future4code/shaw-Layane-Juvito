de11464caef48ca14188add53e4302bcb9fe2c42
LayaneB <50851374+LayaneB@users.noreply.github.com>
2022-05-11 00:15:12 -0300

    Página de erro

--- modulo3/projeto-labeddit/labeddit/src/routes/router.js
+++ modulo3/projeto-labeddit/labeddit/src/routes/router.js
@@ -1,4 +1,5 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import ErrorPage from '../pages/error/ErrorPage'
 import FeedPage from '../pages/feed/FeedPage'
 import LoginPage from '../pages/login/LoginPage'
 import PostPage from '../pages/post/PostPage'
@@ -13,7 +14,7 @@ const Router = () => {
                 <Route path='/signup' element={<SignUpPage />} />
                 <Route path='/feed' element={<FeedPage />} />
                 <Route path='/post/:id' element={<PostPage />} />
-                <Route path='*' element={<h1>Deu ruim!</h1>} />
+                <Route path='*' element={<ErrorPage />} />
             </Routes>
         </BrowserRouter>
     )
