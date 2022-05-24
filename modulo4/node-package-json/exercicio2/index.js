4c3b7de1121acd7d269156bdde6732ddbe234eb1
LayaneB <50851374+LayaneB@users.noreply.github.com>
2022-05-24 19:21:37 -0300

    desafio 3

--- modulo4/node-package-json/exercicio2/index.js
+++ modulo4/node-package-json/exercicio2/index.js
@@ -7,22 +7,22 @@ if(firstNumber && secondNumber){
 
     switch(mathOperation){
         case 'add':
-            console.log("\u001b[32m Resposta da soma:", firstNumber + secondNumber)
+            console.log("\u001b[35m Resposta da soma:", firstNumber + secondNumber)
             break;
         case 'sub':
-            console.log("\u001b[33m Resposta da subtração:", firstNumber - secondNumber)
+            console.log("\u001b[35m Resposta da subtração:", firstNumber - secondNumber)
             break;
         case 'mult':
             console.log("\u001b[35m Resposta da multiplicação:", firstNumber * secondNumber)
             break;
         case 'div':
-            console.log("\u001b[36m Resposta da divisão:", firstNumber / secondNumber)
+            console.log("\u001b[35m Resposta da divisão:", firstNumber / secondNumber)
             break;
         default:
-            console.log("\030[31m Verifique se você está passando uma operação válida: add, sub, mult, div")
+            console.log("\033[41;1;32m Verifique se você está passando uma operação válida: add, sub, mult, div. \033[0m")
             break;
     }
  
 } else {
-    console.log("\030[31m Por favor, você deve inserir dois números após a operação")
+    console.log("\033[41;1;32m Por favor, você deve inserir dois números após a operação \033[0m")
 }
