6522432f2a96767e1bea47d0830cdf504dc5e148
LayaneB <50851374+LayaneB@users.noreply.github.com>
2022-05-25 15:40:53 -0300

    ex2 tinha um console a mais

--- modulo4/intro-typescript/src/exercicio2.ts
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -2,4 +2,4 @@ function imprimeTresCoresFavoritas(cor1:string,cor2:string,cor3:string):void{
     console.log([cor1, cor2, cor3])
 }
 
-console.log(imprimeTresCoresFavoritas('verde','preto','azul'))
+imprimeTresCoresFavoritas('verde','preto','azul')
