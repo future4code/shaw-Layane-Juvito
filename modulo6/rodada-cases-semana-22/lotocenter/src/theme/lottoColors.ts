7cf9bcf88762fb9771cd05e6a9f8593b03e16685
LayaneB <50851374+LayaneB@users.noreply.github.com>
2022-07-19 11:51:45 -0300

    cores de cada loteria da app

---
+++ modulo6/rodada-cases-semana-22/lotocenter/src/theme/lottoColors.ts
@@ -0,0 +1,8 @@
+export enum lottoColors {
+    MEGASENA= '#6BEFA3',
+    QUINA = '#8666EF',
+    LOTOFACIL = '#DD7AC6',
+    LOTOMANIA = '#FFAB64',
+    TIMEMANIA = '#5AAD7D',
+    DIADESORTE = '#BFAF83'
+}
